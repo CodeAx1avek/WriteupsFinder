a18f39dfdb99beefdad0f40ab1b5d66300de8345
CodeAx1 <122358662+CodeAx1avek@users.noreply.github.com>
2024-12-28 01:18:20 +0530

    Add files via upload

---
+++ writeups.js
@@ -0,0 +1,300 @@
+var data = [
+    {
+      title:
+        "Lightning Components: A Treatise on Apex Security from an External Perspective",
+      link: "https://go.appomni.com/hubfs/Collateral/AppOmni_Labs_White_Paper_Apex_Security.pdf",
+    },
+    {
+      title:
+        "How I Found A Vulnerability To Hack iCloud Accounts and How Apple Reacted To It",
+      link: "https://thezerohack.com/apple-vulnerability-bug-bounty",
+    },
+    {
+      title: "Full Local File Read via Error Based XXE using XLIFF File",
+      link: "https://pwn.vg/articles/2021-06/local-file-read-via-error-based-xxe",
+    },
+    {
+      title:
+        "CVE-2021–34506: How We Are Able To Hack Any Company By Sending Message - $20,000 Bounty",
+      link: "https://cyberxplore.medium.com/how-we-are-able-to-hack-any-company-by-sending-message-including-facebook-google-microsoft-b7773626e447",
+    },
+    {
+      title:
+        "Dependency Confusion: How I Hacked Into Apple, Microsoft and Dozens of Other Companies",
+      link: "https://medium.com/@alex.birsan/dependency-confusion-4a5d60fec610",
+    },
+    {
+      title:
+        "Unauthorized Google Maps API Key Usage Cases, and Why You Need to Care",
+      link: "https://ozguralp.medium.com/unauthorized-google-maps-api-key-usage-cases-and-why-you-need-to-care-1ccb28bf21e",
+    },
+    {
+      title: "Awesome host header injection worth 2k",
+      link: "https://medium.com/@imunissar786/awesome-host-header-injection-worth-2k-a7e5be1dbb1d",
+    },
+    {
+      title:
+        "How I was rewarded a $1000 bounty after abusing File Upload functionality to Stored XSS Vulnerability leading to credential theft of a vistor in a website.",
+      link: "https://kunalkhubchandani.medium.com/how-i-was-rewarded-a-1000-bounty-after-abusing-file-upload-functionality-to-stored-xss-945a40ac6f94",
+    },
+    {
+      title:
+        "Let’s know How I have explored the buried secrets in React Native application",
+      link: "https://secureitmania.medium.com/lets-know-how-i-have-explored-the-buried-secrets-in-react-native-application-6236728198f7",
+    },
+    {
+      title: "Strange Admin Panel Bypass Story | | Bug Bounty",
+      link: "https://geekboyranjeet.medium.com/strange-admin-panel-bypass-story-bug-bounty-5e618099baaf",
+    },
+    {
+      title: "Finding 0day to hack Apple",
+      link: "https://github.com/httpvoid/writeups/blob/main/Apple-RCE.md",
+    },
+    {
+      title: "Attack of the clones 2: Git CLI remote code execution strikes back",
+      link: "https://blog.blazeinfosec.com/attack-of-the-clones-2-git-command-client-remote-code-execution-strikes-back/",
+    },
+    {
+      title: "How I hijacked the top-level domain of a sovereign state",
+      link: "https://labs.detectify.com/2021/01/15/how-i-hijacked-the-top-level-domain-of-a-sovereign-state/",
+    },
+    {
+      title:
+        "Insertion Of Malicious Links For Execution In Profile Picture - Unvalidated User Input In MS Sharepoint 2019 (CVE-2020-1456)",
+      link: "https://slashcrypto.org/2021/01/15/CVE-2020-1456/",
+    },
+    {
+      title:
+        "Irremovable Facebook group album photos and entire album under certain circumstances (Bounty: 1000 USD)",
+      link: "https://theshubh77.medium.com/irremovable-facebook-group-album-photos-and-entire-album-under-certain-circumstances-bounty-1000-b1b2a870b8e0",
+    },
+    {
+      title: "Tale of 2 TOOTB Bugs: Google and WhatsApp",
+      link: "https://medium.com/bug-bounty-hunting/tale-of-2-tootb-bugs-google-and-whatsapp-3c0ad40d604c",
+    },
+    {
+      title:
+        "How I managed to trigger a Stored-XSS in an online store with the help of Cache Poisoning",
+      link: "https://www.cysek.org/post/sxss-by-cache-poison-attack",
+    },
+    {
+      title: "Story of a really cool SSRF bug.",
+      link: "https://vedanttekale20.medium.com/story-of-a-really-cool-ssrf-bug-cf88a3800efc",
+    },
+    {
+      title:
+        "Making Clouds Rain :: Remote Code Execution in Microsoft Office 365",
+      link: "https://srcincite.io/blog/2021/01/12/making-clouds-rain-rce-in-office-365.html",
+    },
+    {
+      title: "Stealing User Information Via XSS Via Parameter Pollution",
+      link: "https://levelup.gitconnected.com/stealing-user-information-via-xss-via-parameter-pollution-7d99b3379e7d",
+    },
+    {
+      title: "CSRF with IDOR - A Deadly Combo",
+      link: "https://shahjerry33.medium.com/csrf-with-idor-a-deadly-combo-203e93967702",
+    },
+    {
+      title: "Unrestricted File Upload",
+      link: "https://binamrapandey.medium.com/unrestricted-file-upload-e95e1c6fb80",
+    },
+    {
+      title: "Guest Blog Post: Leaking silhouettes of cross-origin images",
+      link: "https://blog.mozilla.org/attack-and-defense/2021/01/11/leaking-silhouettes-of-cross-origin-images/",
+    },
+    {
+      title: "Stealing Your Private YouTube Videos, One Frame at a Time",
+      link: "https://bugs.xdavidhu.me/google/2021/01/11/stealing-your-private-videos-one-frame-at-a-time/",
+    },
+    {
+      title: "Weblogic Remote Code Execution (Exploiting CVE-2019-2725)",
+      link: "https://blog.cybercastle.io/weblogic-remote-code-execution-exploiting-cve-2019-2725/",
+    },
+    {
+      title: "Unauthorized Access to OData Entities + $2K Bounty From Microsoft",
+      link: "https://medium.com/bugbountywriteup/unauthorized-access-to-odata-entities-2k-bounty-from-microsoft-e070b2ef88c2",
+    },
+    {
+      title:
+        "How I was able to Regain access to account deleted by Admin leading to $$$",
+      link: "https://rajeshranjan457.medium.com/how-i-was-able-to-regain-access-to-account-deleted-by-admin-leading-to-a2c29025f8cd",
+    },
+    {
+      title: "A ‘Novel’ Way to Bypass Executable Signature Checks with Electron",
+      link: "https://parsiya.net/blog/2021-01-08-a-novel-way-to-bypass-executable-signature-checks-with-electron/",
+    },
+    {
+      title: "Create post on any Facebook page",
+      link: "https://www.darabi.me/2020/12/create-invisible-post-on-any-facebook.html",
+    },
+    {
+      title: "$500 getClass",
+      link: "https://sites.google.com/site/testsitehacking/-500-getclass",
+    },
+    {
+      title: "Applying Offensive Reverse Engineering to Facebook Gameroom",
+      link: "https://spaceraccoon.dev/applying-offensive-reverse-engineering-to-facebook-gameroom",
+    },
+    {
+      title:
+        "Access developer tasks list of any Facebook Application (GraphQL IDOR)",
+      link: "https://amineaboud.medium.com/access-developer-tasks-list-of-any-of-facebook-application-graphql-idor-62307c5e5b34",
+    },
+    {
+      title:
+        "Disclose the FB profile of Facebook employees who create official announcement messages (Bug Bounty)",
+      link: "https://amineaboud.medium.com/disclose-the-fb-profile-of-facebook-employees-who-create-official-announcement-messages-bug-76554068caf7",
+    },
+    {
+      title:
+      "500$: MFA bypass By Race Condition",
+      link: "https://infosecwriteups.com/500-mfa-bypass-by-race-condition-176421462902"
+    }
+  ];
+  var count = 0;
+  var t0 = (t1 = 0);
+  
+  $("#txt-search").keyup(function () {
+    var cresult = [];
+    t0 = performance.now();
+    var searchField = $(this).val();
+    if (searchField === "") {
+      $("#filter-records").html("");
+      return;
+    }
+  
+    var regex = new RegExp(searchField, "i");
+    var output = '<div class="row grid divide-y divide-gray-500">';
+    $.each(data, function (key, val) {
+      if (val.title.search(regex) != -1 || val.link.search(regex) != -1) {
+        output += `<div class="row antialiased p-2">
+            <a href=${val.link} target="_blank" rel="noopener nofollow noreferrer">
+  ✍️ ${val.title}</a>
+                        </div>`;
+        cresult.push({
+          title: `${val.title}`,
+          link: `${val.link}`,
+        });
+        count++;
+      }
+    });
+    output += `</div>`;
+  
+    $("#filter-records").html(output);
+    t1 = performance.now();
+    $("#filter-records").prepend(
+      `<div class="text-bold text-center mb-8">Found <code>${
+        cresult.length
+      }</code> resources related to <b>${removeTags(searchField)}</b> in <code>${
+        t1 - t0
+      } ms</code></div>`
+    );
+  });
+  
+  // hashtag clicks
+  
+  $(".searchfilter").click(function () {
+    output = search($(this).val());
+    $("#filter-records").html(output);
+  });
+  
+  function search(filter) {
+    var regex = new RegExp(filter, "i");
+    var output = '<div class="row grid divide-y divide-gray-500">';
+    $.each(data, function (key, val) {
+      if (val.title.search(regex) != -1 || val.link.search(regex) != -1) {
+        output += `<div class="row antialiased p-2">
+    <a href=${val.link} target="_blank" rel="noopener nofollow noreferrer">
+  ✍️ ${val.title}</a>
+                </div>`;
+      }
+    });
+    return output;
+  }
+  
+  // remove banner
+  
+  function Remove(el) {
+    var element = el;
+    element.remove();
+  }
+  
+  // query url param
+  
+  query = new URLSearchParams(window.location.search);
+  param = query.get("q");
+  if (param.length > 1) {
+    output = search(param);
+    $("#filter-records").html(output);
+    $("#filter-records").prepend(
+      `<div class="text-bold text-center mb-8">Showing resources related to <b>${removeTags(
+        param
+      )}</b></div>`
+    );
+  }
+  
+  // json
+  function jsonify(filter) {
+    var regex = new RegExp(filter, "i");
+    var res = [];
+    $.each(data, function (key, val) {
+      if (val.title.search(regex) != -1 || val.link.search(regex) != -1) {
+        res.push({
+          title: `${val.title}`,
+          link: `${val.link}`,
+        });
+      }
+    });
+    return res;
+  }
+  
+  // JSON
+  function downloadObjectAsJson(filter) {
+    if ($("#txt-search").val().length > 0) {
+      var exportData = jsonify($("#txt-search").val());
+      var dataStr =
+        "data:text/json;charset=utf-8," +
+        encodeURIComponent(JSON.stringify(exportData));
+      var downloadAnchorNode = document.createElement("a");
+      downloadAnchorNode.setAttribute("href", dataStr);
+      downloadAnchorNode.setAttribute(
+        "download",
+        `${$("#txt-search").val()}.json`
+      );
+      document.body.appendChild(downloadAnchorNode); // required for firefox
+      downloadAnchorNode.click();
+      downloadAnchorNode.remove();
+    }
+  }
+  
+  // Sanitize Html
+  
+  var tagBody = "(?:[^\"'>]|\"[^\"]*\"|'[^']*')*";
+  
+  var tagOrComment = new RegExp(
+    "<(?:" +
+      // Comment body.
+      "!--(?:(?:-*[^->])*--+|-?)" +
+      // Special "raw text" elements whose content should be elided.
+      "|script\\b" +
+      tagBody +
+      ">[\\s\\S]*?</script\\s*" +
+      "|style\\b" +
+      tagBody +
+      ">[\\s\\S]*?</style\\s*" +
+      // Regular name
+      "|/?[a-z]" +
+      tagBody +
+      ")>",
+    "gi"
+  );
+  
+  function removeTags(html) {
+    var oldHtml;
+    do {
+      oldHtml = html;
+      html = html.replace(tagOrComment, "");
+    } while (html !== oldHtml);
+    return html.replace(/</g, "&lt;");
+  }
+  
